47901d1c750dd8382852194a5a108015b1a12248
Erick Mendez <erick.mendez@icg.tugraz.at>
2006-05-17 12:44:49 +0000

    Fixed VS7 compatibility

--- include/stb/base/fixWin32Issues.h
+++ include/stb/base/fixWin32Issues.h
@@ -1,4 +1,4 @@
-#ifdef WIN32
+#if defined(WIN32)&&(_MSC_VER>=1400)
   // somehow ::TryEnterCriticalSection() is not declared in release build...
   // (needed by ACE)
   BOOL TryEnterCriticalSection(LPCRITICAL_SECTION lpCriticalSection);
